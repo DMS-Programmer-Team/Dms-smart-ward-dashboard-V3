c8fbf3a1e5c8c4d46b7b06240c23d96d08a19d38
Saksitorn <saksitorn04@gmail.com>
2026-01-10 12:57:30 +0700

    fix: env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  APIEndpoint: "http://192.168.15.254/api/udh/delivery",
-  socketAPI: "http://localhost:8500",
+  APIEndpoint: "http://192.168.101.251/api/udh/delivery",
+  socketAPI: "http://192.168.101.251",
   USER_KEY: "userData",
 };
   // socketAPI: "http://localhost:8500",
